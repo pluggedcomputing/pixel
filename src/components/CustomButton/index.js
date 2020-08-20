59d69dcc36bf4dcbf6636ccd2f4f7ae258c00591
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-08-20 16:02:58 -0300

    feat(#4): criar index de botão customizado

---
+++ src/components/CustomButton/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Text, TouchableOpacity, Image} from 'react-native';
+
+import {PropTypes} from 'prop-types';
+
+import tetris01 from '../../assets/imagens/tetris01.png';
+import tetris02 from '../../assets/imagens/tetris02.png';
+import styles from './styles';
+
+const CustomButton = (props) => {
+  const {text, style} = props;
+
+  return (
+    <TouchableOpacity {...props} style={[styles.container, style]}>
+      <Image source={tetris01} style={styles.image} />
+      <Text style={styles.textButton}>{text}</Text>
+      <Image source={tetris02} style={styles.image} />
+    </TouchableOpacity>
+  );
+};
+
+CustomButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
+
+CustomButton.defaultProps = {
+  style: null,
+};
+
+export default CustomButton;
