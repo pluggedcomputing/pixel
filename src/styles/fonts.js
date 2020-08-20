aff36154a6063976fcb74a3abad464eb930f96b4
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-08-20 10:43:12 -0300

    feat(#1): renomear nome de fontes

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,13 +1,11 @@
 import metrics from './metrics';
 
 const fonts = {
-  header: metrics.screenWidth * 0.06,
-  title: metrics.screenWidth * 0.05,
-  input: metrics.screenWidth * 0.04,
-  regular: metrics.screenWidth * 0.035,
-  medium: metrics.screenWidth * 0.03,
-  small: metrics.screenWidth * 0.025,
-  tiny: metrics.screenWidth * 0.02,
+  bigger: metrics.screenHeight * 0.08,
+  regular: metrics.screenWidth * 0.06,
+  medium: metrics.screenWidth * 0.05,
+  small: metrics.screenWidth * 0.04,
+  tiny: metrics.screenWidth * 0.035,
 };
 
 export default fonts;
