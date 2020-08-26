a9a97ee43450310267d717919c0e45cfcacf5429
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-08-26 11:03:00 -0300

    feat(#6): add navigation Main to ScreenAbout

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -5,7 +5,7 @@ import logo from '../../assets/images/logo_name.png';
 import CustomButton from '../../components/CustomButton/index';
 import styles from './styles';
 
-function Main() {
+function Main({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       <Image source={logo} style={styles.logo} />
@@ -18,7 +18,7 @@ function Main() {
       <CustomButton
         text="sobre"
         style={styles.buttons}
-        onPress={() => console.log('clicou sobre')}
+        onPress={() => navigation.navigate('ScreenAbout')}
       />
     </SafeAreaView>
   );
