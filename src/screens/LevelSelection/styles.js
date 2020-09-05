2ce5151a051a142d7592ca8a47a8902a17ec1b2d
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-09-04 23:00:55 -0300

    feat(#9): criar estilização da tela de selação de niveis

---
+++ src/screens/LevelSelection/styles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+import {metrics, colors} from '../../styles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    width: metrics.screenWidth,
+    height: metrics.screenHeight,
+    backgroundColor: colors.colorSecondary,
+  },
+  scrollView: {
+    flexDirection: 'row',
+  },
+  boxContainer: {
+    margin: metrics.doubleBaseMargin,
+    width: metrics.screenWidth * 0.4,
+    height: metrics.screenHeight * 0.9,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  imagePixel: {
+    margin: metrics.smallMargin,
+    width: metrics.screenWidth * 0.15,
+    height: metrics.screenHeight * 0.1,
+  },
+});
+
+export default styles;
