870ab464ce4d7a26c634cce853b90a97c0671b3a
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-09-13 20:45:40 -0300

    feat(#10): adicionando prop light para mudar o botao

--- src/components/ChoiceButton/index.js
+++ src/components/ChoiceButton/index.js
@@ -7,8 +7,9 @@ import { colors } from '../../styles';
 import styles from './styles';
 
 const ChoiceButton = props => {
-  const { correct, onPress, text, step } = props;
-  const [ backgroundColor, setBackgroundColor ] = useState(colors.colorPrimary)
+  const { correct, onPress, text, step, light } = props;
+  const [ backgroundColor, setBackgroundColor] = useState(colors.colorPrimary);
+  const [ color, setTextColor] = useState(colors.colorTextSecondary)
 
   useEffect(() => {
     setBackgroundColor(colors.colorPrimary);
@@ -23,10 +24,18 @@ const ChoiceButton = props => {
     setTimeout(() => onPress(), 350);
   }
 
+  // eslint-disable-next-line no-unused-vars
+  function isLight () {
+    if(light) {
+      setBackgroundColor(colors.colorSecondary);
+      setTextColor(colors.colorPrimary);
+    }
+  }
+
   return (
     <View {...props} style={styles.container}>
       <TouchableOpacity style={[styles.button, { backgroundColor }]} onPress={onPressButton}>
-        <Text style={styles.text}>
+        <Text style={[styles.text, {color}]}>
           {text}
         </Text>
       </TouchableOpacity>
@@ -36,6 +45,7 @@ const ChoiceButton = props => {
 
 ChoiceButton.propTypes = {
   correct: PropTypes.bool,
+  light: PropTypes.bool,
   onPress: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
   step: PropTypes.number.isRequired,
@@ -44,6 +54,7 @@ ChoiceButton.propTypes = {
 
 ChoiceButton.defaultProps = {
   correct: false,
+  light: false
 }
 
 export default ChoiceButton;
