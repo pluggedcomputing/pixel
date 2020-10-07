d79c58d7e47668d2df338ea9350b5000b62e0dec
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-10-07 19:07:07 -0300

    feat(#15): stylizing screen and adding button

--- src/screens/Congratulations/index.js
+++ src/screens/Congratulations/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {
   SafeAreaView,
   StatusBar,
+  TouchableOpacity,
   View,
   Text
 } from 'react-native';
@@ -9,18 +10,36 @@ import {
 import Lottie from 'lottie-react-native';
 
 import animation from '../../assets/animations/CheckedDone.json';
+import styles from './styles';
 
-const Congratulations = () => {
+const Congratulations = props => {
+  const { navigation } = props;
+
+  const navigateScreen = () => {
+    navigation.navigate('LevelSelection');
+  };
+
+  useEffect(() => {
+    setTimeout(navigateScreen, 3000);
+  },[]);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar
         barStyle="light-content"
-        backgroundColor
+        backgroundColor="rgba(0, 0, 0, 0.8)"
       />
-      <View>
-        <Text>Você concluiu o nível</Text>
+      <View style={styles.content}>
+        <Text style={styles.textTop}>Você concluiu o nível X</Text>
         <Lottie source={animation} autoPlay loop />
+        <Text style={styles.textEnd}>Parabéns</Text>
       </View>
+      <TouchableOpacity
+        style={styles.buttonAlternative}
+        onPress={navigateScreen}
+      >
+        <Text style={styles.textButton}>Continuar</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
