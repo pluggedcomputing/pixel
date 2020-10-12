00e4b3eaf71923006a72b6c9c9caed2a8ba08857
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-12 15:25:44 -0300

    feat(#16): criar componente de multiplos escolhas

---
+++ src/components/Questions/MultipleChoice.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import PropTypes from 'prop-types';
+
+import ChoiceButton from '../ChoiceButton';
+import styles from './styles';
+
+const MultipleChoice = (props) => {
+  const {step, setSteps, alternatives} = props;
+  return (
+    <View style={styles.container}>
+      {alternatives.map((item) => (
+        <ChoiceButton
+          key={item.text}
+          step={step}
+          text={item.text}
+          correct={item.correct}
+          onPress={() => {
+            if (item.correct) setSteps(step + 1);
+          }}
+          light
+        />
+      ))}
+    </View>
+  );
+};
+
+MultipleChoice.propTypes = {
+  alternatives: PropTypes.arrayOf(PropTypes.object).isRequired,
+  step: PropTypes.number.isRequired,
+  setSteps: PropTypes.func.isRequired,
+};
+
+export default MultipleChoice;
