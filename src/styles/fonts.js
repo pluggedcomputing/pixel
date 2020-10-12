ad46adf3183b9e8b374a45d3eb3a2512f44887e2
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-12 15:37:21 -0300

    fix(#17): corrigido dimenasão da propriedade bigger

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,7 +1,7 @@
 import metrics from './metrics';
 
 const fonts = {
-  bigger: metrics.screenHeight * 0.08,
+  bigger: metrics.screenWidth * 0.08,
   regular: metrics.screenWidth * 0.06,
   medium: metrics.screenWidth * 0.05,
   small: metrics.screenWidth * 0.04,
