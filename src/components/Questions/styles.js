07df5b03250db5cd97877ac801c968411a18f9dd
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-12 15:27:43 -0300

    feat(#16): refatorar propriedade container

--- src/components/Questions/styles.js
+++ src/components/Questions/styles.js
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-import {metrics, colors} from '../../styles';
+import {metrics} from '../../styles';
 
 const HEIGHT_PORCENTAGE = 0.3;
 const WIDTH_PORCENTAGE = 0.5;
@@ -8,20 +8,12 @@ const WIDTH_PORCENTAGE = 0.5;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: colors.colorAccent,
   },
   logo: {
     resizeMode: 'contain',
     width: metrics.screenWidth * WIDTH_PORCENTAGE,
     height: metrics.screenHeight * HEIGHT_PORCENTAGE,
-  },
-  contianerMessage: {
-    backgroundColor: colors.colorSecondaryLight,
-    width: metrics.screenWidth * 0.8,
-    padding: metrics.basePadding,
-    margin: metrics.doubleBaseMargin,
   },
 });
 
