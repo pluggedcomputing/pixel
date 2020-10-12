1331c2e163ac805cf040a5ef5ac4004b82b4d6c8
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-11 21:02:58 -0300

    feat(#16): adiconar propriedade containerMessage

--- src/components/Questions/styles.js
+++ src/components/Questions/styles.js
@@ -1,20 +1,27 @@
 import {StyleSheet} from 'react-native';
 
-import {metrics} from '../../styles';
+import {metrics, colors} from '../../styles';
 
 const HEIGHT_PORCENTAGE = 0.3;
 const WIDTH_PORCENTAGE = 0.5;
 
 const styles = StyleSheet.create({
   container: {
-    height: metrics.screenHeight * WIDTH_PORCENTAGE,
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'space-around',
     alignItems: 'center',
+    backgroundColor: colors.colorAccent,
   },
   logo: {
     resizeMode: 'contain',
     width: metrics.screenWidth * WIDTH_PORCENTAGE,
     height: metrics.screenHeight * HEIGHT_PORCENTAGE,
+  },
+  contianerMessage: {
+    backgroundColor: colors.colorSecondaryLight,
+    width: metrics.screenWidth * 0.8,
+    padding: metrics.basePadding,
+    margin: metrics.doubleBaseMargin,
   },
 });
 
