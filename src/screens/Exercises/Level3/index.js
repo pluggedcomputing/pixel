2a864f31438219f1a2094b199d192d2fbf36ab48
emanoelmedeiros <emanoel.silva@dcx.ufpb.br>
2020-12-21 14:11:44 -0300

    feat(#7): adicionar mais questões e componente multipleChoice

--- src/screens/Exercises/Level3/index.js
+++ src/screens/Exercises/Level3/index.js
@@ -1,91 +1,242 @@
-import React from 'react';
-import {StatusBar, View, Text, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StatusBar, View, Text} from 'react-native';
 
 import BoxBackground from '../../../components/BoxBackground';
-import ChoiceButton from '../../../components/ChoiceButton';
 import PaintingTable from '../../../components/PaintingTable';
+import {MultipleChoice} from '../../../components/Questions';
 import {colors} from '../../../styles';
 import styles from './styles';
 
-const DATA = [
-  {
-    id: '1',
-    title: 'Planeta',
-    correct: false,
-  },
-  {
-    id: '2',
-    title: 'Rosto',
-    correct: false,
-  },
-  {
-    id: '3',
-    title: 'Xícara',
-    correct: true,
-  },
-  {
-    id: '4',
-    title: 'Coração',
-    correct: false,
-  },
-];
-
 const Level3 = ({navigation}) => {
-  const renderItem = ({item}) => (
-    <View style={styles.contentContainerStyle}>
-      <ChoiceButton
-        style={styles.buttons}
-        text={item.title}
-        correct={item.correct}
-        onPress={() =>
-          item.correct &&
-          navigation.navigate('Congratulations', {
-            level: 3,
-            content: [
-              'Entende como uma imagem criada por você pode ser representada com números binários.',
-            ],
-          })}
-        step={1}
-      />
-    </View>
-  );
-  const BoxBackgroundContent = () => {
-    const content = [
-      <View style={styles.viewBoxContent}>
-        <Text style={styles.textBoxContent}>
-          Pinte os quadrados de acordo com a numeração de cada linha
-        </Text>
-        <View style={{flexDirection: 'row'}}>
-          <PaintingTable content={[]} size={8} />
-          <View>
-            <Text style={styles.textBoxContent}>1,1,1,1,1,1,0,0</Text>
-            <Text style={styles.textBoxContent}>1,1,1,1,1,1,1,1</Text>
-            <Text style={styles.textBoxContent}>1,1,1,1,1,1,0,1</Text>
-            <Text style={styles.textBoxContent}>1,1,1,1,1,1,1,1</Text>
-            <Text style={styles.textBoxContent}>1,1,1,1,1,1,0,0</Text>
-            <Text style={styles.textBoxContent}>0,1,1,1,1,0,0,0</Text>
-            <Text style={styles.textBoxContent}>0,0,1,1,0,0,0,0</Text>
-            <Text style={styles.textBoxContent}>1,1,1,1,1,1,1,1</Text>
-          </View>
-        </View>
+  const [step, setSteps] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+
+  const response = {
+    level: 2,
+    introductions: [
+      {
+        id: 1,
+        text:
+          'Você aprendeu que podemos representar uma imagem através de pontos pretos que possuem os valores 0 e 1, mas podemos ainda representar de outra forma. Os valores que são a representação da imagem pode ser reduzidos, assim tendo memos valores para representalas.',
+        img: null,
+      },
+      {
+        id: 2,
+        text:
+          'Para armazenar uma imagem no computador economizando espaço, basta armazenar quais pontos são pretos e quais pontos são brancos. Vamos aprender como isso funciona!',
+        img: null,
+      },
+    ],
+    questions: [
+      {
+        type: '',
+        enable: true,
+        invisibleRow: -1,
+        description:
+          'Veja abaixo como representar a letra T. A primeira linha consiste de 0 pixels brancos, seguidos de 5 pixels pretos e as linhas seguintes de dois pixels brancos, 1 preto e 2 brancos.',
+
+        paintContent: [
+          '0, 5',
+          '2, 1, 2',
+          '2, 1, 2',
+          '2, 1, 2',
+          '2, 1, 2',
+          '2, 1, 2',
+        ],
+        alternatives: [
+          {
+            id: '1',
+            text: 'Próximo',
+            correct: true,
+          },
+        ],
+      },
+      {
+        type: '',
+        enable: true,
+        invisibleRow: -1,
+        description:
+          'Que letra está sendo representada no código abaixo? Você pode pintar os quadrados na cor preta ou branca clicando neles.',
+        paintContent: [
+          '1, 1, 3',
+          '1, 1, 3',
+          '1, 1, 3',
+          '1, 1, 3',
+          '1, 1, 3',
+          '1, 3, 1',
+        ],
+        alternatives: [
+          {
+            id: '1',
+            text: 'I',
+            correct: false,
+          },
+          {
+            id: '2',
+            text: 'J',
+            correct: false,
+          },
+          {
+            id: '3',
+            text: 'L',
+            correct: true,
+          },
+          {
+            id: '4',
+            text: 'K',
+            correct: false,
+          },
+        ],
+      },
+      {
+        type: '',
+        enable: true,
+        invisibleRow: -1,
+        description:
+          'Que letra está sendo representada no código abaixo? Você pode pintar os quadrados na cor preta ou branca clicando neles.',
+        paintContent: [
+          '5',
+          '0, 1, 2, 1',
+          '0, 1, 1, 1, 2',
+          '0, 2, 3',
+          '0, 1, 1, 1, 2',
+          '0, 1, 2, 1',
+        ],
+
+        alternatives: [
+          {
+            id: '1',
+            text: 'K',
+            correct: true,
+          },
+          {
+            id: '2',
+            text: 'L',
+            correct: false,
+          },
+          {
+            id: '3',
+            text: 'J',
+            correct: false,
+          },
+          {
+            id: '4',
+            text: 'I',
+            correct: false,
+          },
+        ],
+      },
+      {
+        type: '',
+        enable: true,
+        invisibleRow: 4,
+        description:
+          'Como você representaria a penutima linha da imagem abaixo?',
+        paintContent: [
+          '1, 3, 1',
+          '2, 1, 2',
+          '2, 1, 2',
+          '2, 1, 2',
+          '0, 1, 1, 1, 2',
+          '0, 3, 2',
+        ],
+
+        alternatives: [
+          {
+            id: '1',
+            text: '1, 1, 2',
+            correct: false,
+          },
+          {
+            id: '2',
+            text: '0, 1, 1, 2',
+            correct: true,
+          },
+          {
+            id: '3',
+            text: '0, 2, 1, 2',
+            correct: false,
+          },
+          {
+            id: '4',
+            text: '0, 1, 2, 1',
+            correct: false,
+          },
+        ],
+      },
+    ],
+  };
+
+  const [exercise] = useState(response);
+  const [question, setQuestion] = useState(exercise.questions[step]);
+  const maxStep = exercise.questions.length;
+  const finishLevel = step === maxStep;
+
+  useEffect(() => {
+    if (finishLevel) {
+      navigation.navigate('Congratulations', {
+        level: exercise.level,
+        content: [
+          'Parabéns por completar o nível',
+          'Entende como uma imagem criada por você pode ser representada com números',
+        ],
+      });
+    } else {
+      setQuestion(exercise.questions[step]);
+    }
+  }, [step]);
+
+  const viewOfContent = () => {
+    const content = exercise.introductions.map((item) => (
+      <View style={styles.containerOfContent}>
+        <Text style={styles.contentText}>{item.text}</Text>
+      </View>
+    ));
+    content.push(
+      <View style={styles.containerOfContent}>
+        <Text style={styles.contentText}>{question.description}</Text>
+        <PaintingTable
+          content={question.paintContent}
+          isContentReduced
+          enable={question.enable}
+          row={6}
+          column={5}
+          invisibleRow={question.invisibleRow}
+        />
       </View>,
-    ];
+    );
     return content;
   };
+
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={colors.colorPrimary} />
       <View style={styles.halfTopView}>
-        <BoxBackground content={BoxBackgroundContent()} />
+        <BoxBackground
+          content={viewOfContent()}
+          style={styles.boxContainer}
+          isLastPage={setIsLastPage}
+        />
       </View>
       <View style={styles.halfBottomView}>
-        <Text style={styles.textAnswer}> Selecione a opção correta</Text>
-        <FlatList
-          style={styles.buttonsContainer}
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
+        {!isLastPage ? (
+          <Text style={styles.defaultText}>
+            Leia atentamente cada questão para que possa responder o que é
+            solicitado em cada exercício. Arraste o card para o lado e verá as
+            próximas instruções.
+          </Text>
+        ) : (
+          <>
+            <Text style={styles.textAnswer}> Selecione a opção correta</Text>
+            <View style={styles.contentContainerStyle}>
+              <MultipleChoice
+                step={step}
+                setSteps={setSteps}
+                alternatives={question.alternatives}
+              />
+            </View>
+          </>
+        )}
       </View>
     </View>
   );
