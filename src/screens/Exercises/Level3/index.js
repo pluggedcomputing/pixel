48d22d9ada3da43f0ed327a777d3a0ab921c58e1
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2021-03-13 10:51:50 -0300

    fix(#82): adjusting question 5 in level 3

--- src/screens/Exercises/Level3/index.js
+++ src/screens/Exercises/Level3/index.js
@@ -86,7 +86,7 @@ const Level3 = ({navigation}) => {
       {
         id: 5,
         type: 'QUEST',
-        enable: true,
+        enable: false,
         invisibleRow: 4,
         description: 'Como você representaria a penúltima linha da imagem abaixo?',
         enableScroll: false,
