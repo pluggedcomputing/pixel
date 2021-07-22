ccd9280ceb195362cc118692f676028750374e32
emanoelmlsilva <emanoel.silva@dcx.ufpb.br>
2021-07-22 18:28:50 -0300

    feat(#125): criado enum para tipos de questoes

---
+++ src/enums/TypeQuestins.js
@@ -0,0 +1,9 @@
+const TypeQuestions = {
+  CHOICEBUTTON: 'choicebutton',
+  MULTIPLECHOICE: 'multiplechoice',
+  PAINTINGTABLE: 'paintingtable',
+  SHOWIMAGE: 'showimage',
+  NONE: 'none',
+};
+
+export default TypeQuestions;
