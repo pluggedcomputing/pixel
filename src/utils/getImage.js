f82eb71934d66ff5f8ff04afc1db29e5346b3d4b
emanoelmlsilva <emanoel.silva@dcx.ufpb.br>
2021-09-13 17:07:12 -0300

    feat(#137): adicionado novas imagem

--- src/utils/getImage.js
+++ src/utils/getImage.js
@@ -3,17 +3,17 @@ const imagens = {
   // nivel1
   l1q1: require('../assets/images/level1/gitComputer.gif'),
   l1q2: require('../assets/images/level1/photoPixel.png'),
-  l1q3: require('../assets/images/level1/ballPixel.png'),
+  l1q3: require('../assets/images/level1/hand.png'),
   l1q4: require('../assets/images/level1/grid.png'),
   l1q5: require('../assets/images/level1/fax.png'),
-  l1q6: require('../assets/images/level1/faxIn.gif'),
-  l1q7: require('../assets/images/level1/faxOut.gif'),
+  l1q6: require('../assets/images/level1/document.gif'),
+  l1q7: require('../assets/images/level1/faxOut.png'),
   // nivel2
   l2q1: require('../assets/images/level2/dog.png'),
   l2q2: require('../assets/images/level2/Cgrade.png'),
   // nivel3
-  l3q1: require('../assets/images/level3/bit.png'),
-  l3q2: require('../assets/images/level3/convert.png'),
+  l3q1: require('../assets/images/level3/heart.png'),
+  l3q2: require('../assets/images/level3/zip.png'),
   l3q3: require('../assets/images/level3/heart.png'),
   l3q4: require('../assets/images/level3/exGrid.png'),
 };
