7360882c02df5de7140ccf9c831bcb97fe42d769
emanoelmlsilva <emanoel.silva@dcx.ufpb.br>
2021-11-03 18:28:04 -0300

    feat(#172): criado variavel para definir valor da divisao

--- src/utils/randomRowValue.js
+++ src/utils/randomRowValue.js
@@ -1,13 +1,13 @@
-  const randomRowValue = (numColumn) => {
-    const MIN = 0;
-    const MAX = 100;
-    const value = [];
-
-    for(let i = 0; i < numColumn; i += 1){
-      const valueRandom = Math.floor(Math.random() * (MAX - MIN)) + MIN;
-      value.push(valueRandom % 2)
-    }
-    return value;
+const randomRowValue = (numColumn) => {
+  const MIN = 0;
+  const MAX = 100;
+  const value = [];
+  const valueDiv = 2;
+  for (let i = 0; i < numColumn; i += 1) {
+    const valueRandom = Math.floor(Math.random() * (MAX - MIN)) + MIN;
+    value.push(valueRandom % valueDiv);
   }
+  return value;
+};
 
-  export default randomRowValue;
+export default randomRowValue;
