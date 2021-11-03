dd20ff7d1d27e4a5a08b3c424a94674ba7a579e5
emanoelmlsilva <emanoel.silva@dcx.ufpb.br>
2021-11-03 18:13:23 -0300

    feat(#172): adicionado opcao para pintar com cores e refatorado codigo de pintar quadrado

--- src/components/PaintingTable/styles.js
+++ src/components/PaintingTable/styles.js
@@ -32,11 +32,14 @@ const styles = StyleSheet.create({
     borderColor: colors.colorPrimary,
   },
   squareChoice: {
-    borderWidth: metrics.baseBorder + 0.5,
-    borderColor: colors.colorAccent,
+    borderWidth: metrics.baseBorder,
+    borderColor: colors.colorSecondaryDark,
   },
   squareColoring: {backgroundColor: colors.colorTextPrimary},
   discolorSquare: {backgroundColor: colors.colorSecondaryLight},
+  squareRed: {backgroundColor: colors.colorError},
+  squareGreen: {backgroundColor: colors.colorSucess},
+  squareBlue: {backgroundColor: colors.colorAccent},
   containerChoiceColor: {
     marginTop: metrics.smallMargin,
     flexDirection: 'row',
@@ -52,6 +55,10 @@ const styles = StyleSheet.create({
   containerTextModify: {
     flexDirection: 'row',
     justifyContent: 'space-around',
+  },
+  containerBoxColor: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 });
 
