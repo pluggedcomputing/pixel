0174c4a4ec5a4dc60d4323d4d92b634391a0a507
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2022-04-08 22:07:59 -0300

    feat(#188): acrescentando seta vetor na barra superior, lado direito

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import logo from './assets/images/horizontal-logo.png';
+import vector from './assets/images/levelSelection/vector.png';
 import Congratulations from './screens/Congratulations';
 import Exercises from './screens/Exercises';
 import LevelSelection from './screens/LevelSelection';
 import Main from './screens/Main';
 import ScreenAbout from './screens/ScreenAbout';
-import {colors} from './styles';
+import {colors, metrics} from './styles';
 
 
 const styles = StyleSheet.create({
@@ -18,10 +19,23 @@ const styles = StyleSheet.create({
     resizeMode:'contain',
     width:140,
     height:50,
+  },
+  vector:{
+    margin: metrics.baseMargin,
   }
 });
 
+
 const Stack = createStackNavigator();
+
+function Vector() {
+  return (
+    <Image
+      style={styles.vector}
+      source={vector}
+    />
+  );
+}
 
 function LogoTitle() {
   return (
@@ -66,9 +80,24 @@ const routes = () => {
         <Stack.Screen
           name="LevelSelection"
           component={LevelSelection}
-          options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+          options={{
+            headerTitle: (props) => <LogoTitle {...props} />,
+            headerLeft: null,
+            headerRight: (props) => <Vector {...props} />
+          }}
         />
-        <Stack.Screen name="Exercises" component={Exercises} options={{headerLeft: null}} />
+        <Stack.Screen
+          name="Exercises"
+          component={Exercises}
+          options={{
+            headerTitleStyle: {
+              fontFamily: 'Poppins-Bold',
+            },
+            headerLeft: null,
+
+          }}
+
+        />
 
         <Stack.Screen
           options={{headerShown: false}}
