6a476cea09815fb2054f25b14498999a24bcf142
emanoelmlsilva <emanoel.silva@dcx.ufpb.br>
2022-04-24 21:14:43 -0300

    feat(#185): adicionado lib codepush e appcenter

--- android/app/src/main/java/br/ufpb/dcx/computacaoplugada/pixel/MainApplication.java
+++ android/app/src/main/java/br/ufpb/dcx/computacaoplugada/pixel/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -34,6 +35,11 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
       };
 
